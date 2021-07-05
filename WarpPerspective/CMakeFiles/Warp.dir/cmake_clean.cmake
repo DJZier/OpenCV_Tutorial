020a70f6ef7ee5cafa97cf67af568a9ee3803231
DJZier <florian.convert64@gmail.com>
2021-07-05 10:44:29 +0200

    chapitre5 warp

---
+++ WarpPerspective/CMakeFiles/Warp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Warp.dir/Warp.cpp.o"
+  "Warp"
+  "Warp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Warp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
